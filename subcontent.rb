be481622494a4646176e2a519aba8974b6658d84
YongJieYongJie <KhooYongJie@gmx.com>
2016-05-21 10:58:09 +0800

    temp

--- subcontent.rb
+++ subcontent.rb
@@ -1,13 +1,21 @@
 require 'nokogiri'
 
 class Subcontent
+  attr_accessor :test_output
+
   def initialize(node)
-    if is_part?(node)
+    if is_subcontent?(node)
+      subcontent = parse_subcontent()
+    elsif !node.children.empty?
 
     end
   end
 
-  def is_part?(node)
+  def is_subcontent?(node)
+    is_part?(node)
+  end
 
+  def is_part?(node)
+    node.at_xpath('../div[@class="part"]')
   end
 end
